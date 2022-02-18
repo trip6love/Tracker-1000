6975416b122aa922472e6475f9c811653c3aa3af
trip6love <lanebyers13@gmail.com>
2022-02-18 14:41:10 -0500

    added seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,17 @@
+INSERT INTO department (name)
+VALUES
+    ('Finance'),
+    ('Engineering'),
+    ('Sales'),
+    ('Legal');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Lawyer' 200000.00, 4),
+    ('Web Developer', 50.00, 2),
+    ('Salesman' 60000.00, 3),
+    ('Finacial Advisor', 500000.00, 1);
+
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
     ('Lane', 'Byers', 1, 2),
