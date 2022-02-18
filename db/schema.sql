1e8c46f7eff7bef83db8424fe7e92e92cb5bbc2c
trip6love <lanebyers13@gmail.com>
2022-02-18 13:55:36 -0500

    started employee schema

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS empolyee;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER REFERENCES employee(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
+);
+
