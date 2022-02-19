3bc6852042adf7b9c5db427c286706abbbcd4610
trip6love <lanebyers13@gmail.com>
2022-02-19 11:54:35 -0500

    last task to finish manage js

--- manage.js
+++ manage.js
@@ -1,3 +1,13 @@
 const inquirer = require('inquirer');
 const { Table } = require('console.table');
 const db = require('./db/connect');
+const { start } = require('repl');
+
+db.connect(err => {
+    if (err) throw err;
+    start();
+});
+
+const start = function () {
+    inquirer
+}
