1518532b7a3424d8110fba42e08d2bf1b7462bdb
trip6love <lanebyers13@gmail.com>
2022-02-20 13:01:56 -0500

    adding start functionality

--- manage.js
+++ manage.js
@@ -10,4 +10,27 @@ db.connect(err => {
 
 const start = function () {
     inquirer
-}
+        .prompt({
+            type: 'list',
+            name: 'Menu',
+            message: 'From the list, choose what to do!',
+            choices: ['View Roles', 'View Departments', 'View Employees', 'Add Employee', ' Update an Employee', 'Add Role', 'Add Department']
+        })
+        .then(response => {
+            if (response.Menu === 'View Roles') {
+                viewRoles();
+            } else if (response.Menu === 'View Departments') {
+                viewDepartments();
+            } else if (response.Menu === 'View Employees') {
+                viewEmployees();
+            } else if (response.Menu === 'Add Employee') {
+                addEmployee();
+            } else if (response.Menu === 'Add Role') {
+                addRole();
+            } else if (response.Menu === 'Add Department') {
+                addDepartment();
+            } else if (response.Menu === 'Update Employee') {
+                updateEmployee();
+            }
+        });
+};
